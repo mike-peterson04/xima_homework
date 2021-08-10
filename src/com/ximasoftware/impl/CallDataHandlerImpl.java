8342325d985d1699d46eb702b1eb0c817d1f84cf
Mike Peterson <mpeterson2004@gmail.com>
2021-08-10 13:54:57 -0600

    final version for turn in

--- src/com/ximasoftware/impl/CallDataHandlerImpl.java
+++ src/com/ximasoftware/impl/CallDataHandlerImpl.java
@@ -1,6 +1,5 @@
 package com.ximasoftware.impl;
 
-import java.lang.reflect.Array;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import com.ximasoftware.CallDataHandler;
@@ -8,8 +7,9 @@ import com.ximasoftware.EventType;
 
 public class CallDataHandlerImpl implements CallDataHandler {
 	//I think storing completed calls in a database would be a more scalable solution but for purposes of this example I am storing them in memory
+	//switched some collections to  Hashtable as ConcurrentHashMap was throwing errors due to multiple simultaneous access requests not being handled correctly
 	Hashtable<String,Call> activeCalls = new Hashtable<String,Call>();
-	ConcurrentHashMap<String,Call> completeCalls = new ConcurrentHashMap<String,Call>();
+	Hashtable<String,Call> completeCalls = new Hashtable<String,Call>();
 	ConcurrentHashMap<String,Long> callTotals;
 	ConcurrentHashMap<String,Party> parties = new ConcurrentHashMap<String,Party>();
 
@@ -88,21 +88,26 @@ public class CallDataHandlerImpl implements CallDataHandler {
 
 	@Override
 	public int getNumberOfActiveCalls() {
-		
+		//return the size of the activeCalls collection
 		return activeCalls.size();
 	}
 
 	@Override
 	public int getNumberOfCompletedCalls() {
+		
+		//return the size of the completeCalls collection
 		return completeCalls.size();
 	}
 
 	@Override
 	public long getTotalEventDuration(EventType type) {
+		
 		if(callTotals == null) {
+			//if we haven't handled a call yet return 0
 			return 0;
 		}
 		try {
+			//trycatch was created because I was having some issues working with EventType had initially used type+"" to convert pass it as String but it didn't always work saw no reason to remove trycatch after issue was resolved
 			return callTotals.get(type.toString());	
 		}
 		catch(Exception e){
@@ -115,18 +120,18 @@ public class CallDataHandlerImpl implements CallDataHandler {
 	@Override
 	public long getTotalCallTimeForParty(String party) {
 		Party agent = parties.get(party);
-		//check to see if a valid party was requested
+		//check to see if a valid party was requested and return 0 if not valid party
 		if (agent == null) {
 			return 0;
 		}
-		
+		//really I think this would be best handled via a DB, but that wouldn't be quick enough for the unit tests and isn't needed at the scale in question for said tests this solution is based on sorting the calls @ creation by a specific Party
+		//we can rapidly find the party due to being stored in a hashmap then iterate through each call and total the current time.
 		return agent.timeTotal();
 	}
 	
 	private void eventManager(Call updater) {
 		//tracking time of completed calls that I think would make more sense to store in a DB as opposed to in memory if this were truly being implemented but keeping in memory for purposes of this assignment
 		if (callTotals == null){
-			
 			long a = 0;
 			callTotals = new ConcurrentHashMap<String,Long>();
 			callTotals.put("DIAL", a);
